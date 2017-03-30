db5c21318ade1b9e5f9f1bb90aa7bdfb4a405f8e
Gabe Sumner <gsumner@gsumner-ltm6.internal.salesforce.com>
2017-03-30 09:31:57 -0700

    Added scores

---
+++ src/main/java/com/herokuapp/odata/workday/entities/Score.java
@@ -0,0 +1,23 @@
+package com.herokuapp.odata.workday.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name="scores")
+public class Score {
+
+	@Id
+	private Integer id;
+
+	private String candidate;
+
+	private String attribute;
+
+	private Integer score;
+
+}
