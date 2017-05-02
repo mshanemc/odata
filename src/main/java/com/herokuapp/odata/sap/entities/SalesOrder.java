5e5e2afee0697084bd0532d1c3870ceff3f360dc
Gabe Sumner <gsumner@gsumner-ltm6.internal.salesforce.com>
2017-05-01 23:41:04 -0700

    Added SAP

---
+++ src/main/java/com/herokuapp/odata/sap/entities/SalesOrder.java
@@ -0,0 +1,32 @@
+package com.herokuapp.odata.sap.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Table(name="salesorders")
+public class SalesOrder {
+
+	@Id
+	private Integer orderid;
+
+	private Integer partnerid;
+
+	private String customername;
+
+	private String status;
+
+	private String currency;
+
+	private BigDecimal netsum;
+
+	private BigDecimal tax;
+
+	private BigDecimal totalsum;
+
+}
