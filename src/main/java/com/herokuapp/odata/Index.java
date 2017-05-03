4cd141222255c5be20df2972d988051081113521
Gabe Sumner <gsumner@gsumner-ltm6.internal.salesforce.com>
2017-05-03 09:40:22 -0700

    Added Index

---
+++ src/main/java/com/herokuapp/odata/Index.java
@@ -0,0 +1,34 @@
+package com.herokuapp.odata;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public final class Index extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>oData Demo Sources</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("<h1>oData Demo Sources</h1>");
+        writer.println("<ul>");
+        writer.println("<li><a href='/dreamhouse'>DreamHouse</a></li>");
+        writer.println("<li><a href='/huzzah'>Huzzah</a></li>");
+        writer.println("<li><a href='/ibm'>IBM</a></li>");
+        writer.println("<li><a href='/sap'>SAP</a></li>");
+        writer.println("<li><a href='/workday'>Workday</a></li>");
+        writer.println("</ul>");
+        writer.println("</body>");
+        writer.println("</html>");
+
+    }
+
+}
