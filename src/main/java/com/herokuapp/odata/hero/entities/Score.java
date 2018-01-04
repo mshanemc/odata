ad50af9cdd6d55db694b9038a09e8349aad692f5
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-01-04 15:35:20 -0600

    add scores for free trial

---
+++ src/main/java/com/herokuapp/odata/hero/entities/Score.java
@@ -0,0 +1,23 @@
+package com.herokuapp.odata.hero.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table(name="scores")
+public class Score {
+
+	@Id
+	private Integer id;
+
+	private Integer candidate;
+
+	private String attribute;
+
+	private Integer score;
+
+}
